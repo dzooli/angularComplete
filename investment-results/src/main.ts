5ad7356cdc3db41ab03f128463d0ec86bb3d7916
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2026-03-16 10:34:46 +0100

    style: update .editorconfig for consistent indentation and enhance error handling in main.ts
    - Changed indentation size from 2 to 4 spaces in .editorconfig for better readability.
    - Improved error handling in main.ts by wrapping the bootstrapApplication call in a try-catch block to log errors more effectively.

--- investment-results/src/main.ts
+++ investment-results/src/main.ts
@@ -1,4 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+try {
+    await bootstrapApplication(AppComponent);
+} catch (error) {
+    console.error(error);
+}
