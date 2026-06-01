9a9de2b6d33b900f5661e6fa1e90a0bdad7c3f3a
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2026-06-01 22:55:31 +0200

    Enhance ControlComponent with click event handling
    - Added a click event listener to the host element of ControlComponent.
    - Implemented onClick method to log a message when the component is clicked.
    - Included deprecated alternatives for adding class and listener for reference.

--- components-deep-dive/01-starting-project/src/app/components/shared/control/control.component.ts
+++ components-deep-dive/01-starting-project/src/app/components/shared/control/control.component.ts
@@ -9,9 +9,21 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'control',
+    '(click)': 'onClick()', // declare a listener to the host element (recommended way)
   },
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control'; // deprecated alternative way to add class to the host element
+  
+  // Alternatively define a deprecated way to add a listener to the host element
+  // @HostListener('click') onClick() {
+  //   console.log('Control component clicked');
+  // }
+
   label = input.required<string>();
   target = input.required<string>();
+
+  onClick() {
+    console.log('Control component clicked');
+  }
 }
